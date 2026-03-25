97b1dfa6fcc085a8374609c685b60e1ffccb1a92
codygettuneski <cody@gettuneski.com>
2026-03-25 14:27:23 -0600

    feat: update InitiateLoginDto to use username and add optional fields
    - Changed the `email` field to `username` in `InitiateLoginDto` for consistency with user identification.
    - Added optional fields `session` and `deviceKey` to enhance flexibility in the login process.
    - Updated validation rules for new fields to ensure proper input handling.

--- src/auth/dto/initiate-login.dto.ts
+++ src/auth/dto/initiate-login.dto.ts
@@ -1,22 +1,31 @@
-import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class InitiateLoginDto {
   @IsEmail()
-  email: string;
+  username: string;
 
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters' })
   password: string;
 
+  @IsOptional()
   @IsString()
   @MinLength(1, { message: 'Session is required' })
   session: string;
 
+  @IsOptional()
   @IsString()
   @MinLength(1, { message: 'Device key is required' })
   @MaxLength(256)
   deviceKey: string;
 
+  @IsOptional()
   @IsString()
   @MinLength(1, { message: 'Device name is required' })
   @MaxLength(256)
