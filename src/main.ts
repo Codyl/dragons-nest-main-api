02cf86d9a943ff014f3c91e303885497bd9aaf3a
codygettuneski <cody@gettuneski.com>
2026-03-25 14:26:11 -0600

    feat: enhance application bootstrap with improved logging and server startup messages
    - Configured NestFactory to use a detailed logger for various log levels including error, warn, log, verbose, and debug.
    - Added console logs to provide feedback on server status, API documentation availability, and the current environment mode upon server startup.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ import { COOKIE_SECRET, FRONTEND_URL, PORT } from 'src/env.constants';
 import { EnvironmentVariables } from 'src/env.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'verbose', 'debug'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -19,6 +22,7 @@ async function bootstrap() {
       },
     }),
   );
+
   const config = app.get(ConfigService<EnvironmentVariables>);
   app.use(cookieParser(config.getOrThrow(COOKIE_SECRET, { infer: true })));
   app.enableCors({
@@ -36,7 +40,16 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   const port = config.getOrThrow(PORT, { infer: true });
-  await app.listen(port ? Number(port) : 8080);
+  await app.listen(port ? Number(port) : 8080).then(() => {
+    console.log(
+      `Server is running on port ${port} in ${process.env.NODE_ENV} mode`,
+    );
+    console.log(
+      `API documentation is available at http://localhost:${port}/api`,
+    );
+    console.log(`API is available at http://localhost:${port}`);
+    console.log(`ENV in use is ${process.env.NODE_ENV}`);
+  });
 }
 
 void bootstrap();
